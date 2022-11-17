8826943de4c159341aba3a4224cd9489424c54eb
roostie-kam <kam22008@byui.edu>
2022-11-17 12:38:45 -0700

    wireframe_stuff

---
+++ Week_9_Challenge/challenge.py
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <meta charset="utf-8">
+    <title>W09 - Challenge</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/prism-light.css" />
+    <link rel="stylesheet" href="../css/course.css" />
+</head>
+
+<body>
+    <div id="content">
+        <header class="headerBanner">
+            <div>
+                <h1>
+                    <span>WEB</span> Fundamentals</h1>
+                <h2>WDD 130</h2>
+            </div>
+        </header>
+        <main>
+            <h1>W09 - Challenge</h1>
+            <hr />
+            <h2>Activity Directions</h2>
+            <p>Estimated time: 40 minutes</p>
+            <p>From the image below create a webpage to look just like it. Try using grid for the layout. The grid doesn't necessarily need to be the whole card but at least the bottom portion of the card. The card below has a small width so it should look good on any screen. The image below is how it would look on a medium size screen. You are welcome to work in groups. The image is provided below.</p>
+            <div class="fig-block">
+                <figure>
+                    <img alt="W09 - ICE" src="https://byui-wdd.github.io/wdd130/ice/w09_example.png"
+                        title="W09 ICE image without hover" />
+                    <figcaption>
+                        W09 image
+                    </figcaption>
+                </figure>
+            </div>
+            <h2>Images:</h2>
+            <a href="https://byui-wdd.github.io/wdd130/ice/w09_ice_img.jpg" target="_blank">City Scape</a>&nbsp
+            <p>For help with superscript like the 'm' by the 5 <a href="https://www.w3schools.com/tags/tag_sup.asp"> See w3schools HTML sup Tag</a></p>
+            <p>You might also want to use overflow: hidden in CSS with the border radius <a href="https://www.w3schools.com/css/css_overflow.asp"> See w3schools Layout Overflow </a></p>
+            <!-- end #article -->
+        </main>
+        <script src="../js/course.js"></script>
+        <script src="../js/prism.js"></script>
+    </div>
+</body>
+
+</html>
